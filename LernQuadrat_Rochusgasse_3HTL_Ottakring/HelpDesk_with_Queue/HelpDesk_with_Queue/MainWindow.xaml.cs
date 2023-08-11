d11fe41b5f5484415f0c18d62a88c8ce70c6b137
Elias Huhsovitz <e12121272@student.tuwien.ac.at>
2023-08-11 16:51:25 +0200

    added beginning of HelpDesk

---
+++ LernQuadrat_Rochusgasse_3HTL_Ottakring/HelpDesk_with_Queue/HelpDesk_with_Queue/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using HelpRequestLibrary; // Library wurde als reference hinzugefügt: HelpDesk_with_Qeue -> Dependencies -> Add project reference
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelpDesk_with_Queue
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Queue<HelpRequest> helpRequestQueue;
+        private HelpRequest currentSupportRequest;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            InitUI();
+        }
+
+        private void initClassVariables()
+        {
+            helpRequestQueue = new Queue<HelpRequest>();
+            currentSupportRequest = new HelpRequest(string.Empty, string.Empty, new DateTime());
+        }
+
+        private void InitUI()
+        {
+            userDpDate.SelectedDate = DateTime.Now;
+        }
+
+        private void userBtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime dt = (DateTime) userDpDate.SelectedDate;
+            helpRequestQueue.Enqueue(new HelpRequest(userTbName.Text, userTbRequest.Text, dt));
+
+            userLbSendConfirmation.Content = "Request is sent to support";
+        }
+
+        private void supportBtnGetNextRequest_Click(object sender, RoutedEventArgs e)
+        {
+            currentSupportRequest = helpRequestQueue.Dequeue();
+
+            supportTbName.Text = currentSupportRequest.name;
+            supportTbDate.Text = currentSupportRequest.date.ToString();
+            supportTbRequest.Text = currentSupportRequest.request;
+        }
+
+        private void supportBtnSendAgain_Click(object sender, RoutedEventArgs e)
+        {
+            helpRequestQueue.Enqueue(new HelpRequest(currentSupportRequest.name, supportTbRequest.Text, currentSupportRequest.date));
+        }
+    }
+}
---
+++ LernQuadrat_Rochusgasse_3HTL_Ottakring/HelpDesk_with_Queue/HelpRequestLibrary/HelpRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace HelpRequestLibrary
+{
+    public class HelpRequest
+    {
+        public string name { get;}
+        public string request { get; }
+        public DateTime date { get; }
+
+        public HelpRequest(string name, string request, DateTime date) { 
+
+            if (name == null || date == null) throw new ArgumentNullException("name & date must not be null");
+            
+            this.name = name;
+            this.request = request;
+            this.date = date;
+        }
+
+
+    }
+}
